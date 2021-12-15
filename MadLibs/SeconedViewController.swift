e5265ef7bad536a9fd3dc037065f9e41274bddf7
fatengh <faten.arg@hotmail.com>
2021-12-15 19:34:36 +0000

    first commit

---
+++ MadLibs/SeconedViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SeconedViewController.swift
+//  MadLibs
+//
+//  Created by admin on 15/12/2021.
+//
+
+import UIKit
+
+class SeconedViewController: UIViewController {
+
+    @IBOutlet weak var adjectiveLebel: UITextField!
+    
+
+    
+    @IBOutlet weak var verb1: UITextField!
+    @IBOutlet weak var verb2: UITextField!
+    @IBOutlet weak var nounLebel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+   
+}
--- MadLibs/ViewController.swift
+++ MadLibs/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLebel: UILabel!
+    
+    @IBOutlet weak var formButton: UIBarButtonItem!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        formButton.tintColor = .white
     }
-
-
+    
+    
+    @IBAction func moveToForm(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "Linked" , sender: self)
+    }
+    
+    @IBAction func unwindMain(_ unwindSegue: UIStoryboardSegue) {
+         let dec = unwindSegue.source as! SeconedViewController
+            textLebel.text = "We are having a perfectly \(dec.adjectiveLebel.text!) time right now. Later we will \(dec.verb1.text!) and \(dec.verb2.text!) in the \(dec.nounLebel.text!)."
+                
+            }
 }
-
